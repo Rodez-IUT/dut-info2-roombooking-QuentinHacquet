950c2d891a67dd75b02357d26883103fcdd24bba
QuentinHacquet <quentin.hacquet@iut-rodez.fr>
2020-03-04 18:14:52 +0100

    fix #1.1 Parser SAX

--- part1-roombooking-xml/src/main/java/xmlws/roombooking/xmltools/RoomBookingSaxParser.java
+++ part1-roombooking-xml/src/main/java/xmlws/roombooking/xmltools/RoomBookingSaxParser.java
@@ -1,10 +1,82 @@
 package xmlws.roombooking.xmltools;
 
-import java.io.InputStream;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+/**
+ * Class for objects responsible of RoomBooking xml files parsing
+ * SAX version
+ */
 public class RoomBookingSaxParser implements RoomBookingParser {
-    @Override
+
+    RoomBooking roomBooking = new RoomBooking();
+    String tmpLocalName;
+
+    /**
+     * Parse an xml file provided as an input stream
+     *
+     * @param inputStream the input stream corresponding to the xml file
+     */
     public RoomBooking parse(InputStream inputStream) {
-        return null;
+
+        try {
+            SAXParserFactory spf = SAXParserFactory.newInstance();
+            spf.setNamespaceAware(true);
+            SAXParser saxParser = spf.newSAXParser();
+            saxParser.parse(inputStream, new RoomBookingHandler());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return roomBooking;
     }
+
+    private class RoomBookingHandler extends DefaultHandler {
+
+        public void startElement(String namespaceURI,
+                                 String localName,
+                                 String qName,
+                                 Attributes atts)
+                throws SAXException {
+            System.out.println("In element: "+localName);
+            tmpLocalName = localName;
+        }
+
+        public void characters(char ch[], int start, int length)
+                throws SAXException {
+            System.out.println(new String(ch, start, length));
+            String data = new String(ch, start, length);
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+
+            if (data.charAt(0) != '\n') {
+                if (tmpLocalName.equals("label")) {
+                    roomBooking.setRoomLabel(data);
+                }
+                if (tmpLocalName.equals("username")) {
+                    roomBooking.setUsername(data);
+                }
+                if (tmpLocalName.equals("startDate")) {
+                    try {
+                        roomBooking.setStartDate(sdf.parse(data));
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (tmpLocalName.equals("endDate")) {
+                    try {
+                        roomBooking.setEndDate(sdf.parse(data));
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
 }
